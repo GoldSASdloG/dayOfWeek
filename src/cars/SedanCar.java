d6b48bb5ef8efaa7d321e152aeb4eda9805bb298
GoldSASdloG <Procatulus@gmail.com>
2024-12-20 16:06:52 +0300

    add TRUCK delete empty method

--- src/cars/SedanCar.java
+++ src/cars/SedanCar.java
@@ -1,8 +1,6 @@
 package cars;
 
 public class SedanCar extends Cars {
-    public SedanCar() {
-    }
 
     public SedanCar(String body, int maxSpeed, String mark, String engine, int volumeOfGasoline, int gasolineConsumption) {
         super(body, maxSpeed, mark, engine, volumeOfGasoline, gasolineConsumption);
